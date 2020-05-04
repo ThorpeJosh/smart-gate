8604f3f942749b7e8f0307615e4fdc747ee92b5d
Josh Thorpe <josh@thorpe.engineering>
2020-05-04 19:16:32 +1000

    Added guide to using wifi dongle

--- install_RTL8188FU_drivers.sh
+++
@@ -1,4 +0,0 @@
-sudo echo "deb http://ppa.launchpad.net/kelebek333/kablosuz/ubuntu bionic main" > /etc/apt/sources.list.d/kelebek333-ubuntu-kablosuz-bionic.list
-sudo apt-get update
-sudo apt install rtl8188fu-dkms
-sudo reboot
