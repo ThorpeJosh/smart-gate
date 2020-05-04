2a8ec977e5187dc760563a51d47dbfe92cfb2d7d
Josh Thorpe <josh@thorpe.engineering>
2020-05-04 12:58:14 +1000

    Install script for wifi radio

--- install_RTL8188FU_drivers.sh
+++ install_RTL8188FU_drivers.sh
@@ -1,4 +1,4 @@
-sudo add-apt-repository ppa:kelebek333/kablosuz
+sudo echo "deb http://ppa.launchpad.net/kelebek333/kablosuz/ubuntu bionic main" > /etc/apt/sources.list.d/kelebek333-ubuntu-kablosuz-bionic.list
 sudo apt-get update
 sudo apt install rtl8188fu-dkms
 sudo reboot
