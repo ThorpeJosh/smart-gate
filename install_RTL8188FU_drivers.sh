b712cd21d72eb7494a04aa8561792377b4bb0ee6
Josh Thorpe <josh@thorpe.engineering>
2020-05-04 13:08:09 +1000

    Install script for wifi radio

--- install_RTL8188FU_drivers.sh
+++ install_RTL8188FU_drivers.sh
@@ -1,4 +1,9 @@
-sudo echo "deb http://ppa.launchpad.net/kelebek333/kablosuz/ubuntu bionic main" > /etc/apt/sources.list.d/kelebek333-ubuntu-kablosuz-bionic.list
-sudo apt-get update
-sudo apt install rtl8188fu-dkms
+#!/bin/bash
+set -e
+sudo apt-get install build-essential git dkms linux-headers-$(uname -r)
+git clone https://github.com/kelebek333/rtl8188fu
+sudo dkms add ./rtl8188fu
+sudo dkms build rtl8188fu/1.0
+sudo dkms install rtl8188fu/1.0
+sudo cp ./rtl8188fu/firmware/rtl8188fufw.bin /lib/firmware/rtlwifi/
 sudo reboot
