8cbd0311a8ba6e97a7c35e88c765041da146ac49
Josh Thorpe <josh@thorpe.engineering>
2020-05-04 23:03:26 +1000

    Fixed up pylint issues

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
+"""Smart gate module entry point
+"""
 import logging
-import time
+#import time
 LOG_FORMAT = '%(levelname)s %(asctime)s : %(message)s'
-logging.basicConfig(filename = 'gate.log', level = logging.DEBUG, format = LOG_FORMAT)
+logging.basicConfig(filename='gate.log', level=logging.DEBUG, format=LOG_FORMAT)
 logger = logging.getLogger()
 
 
 class Gate():
+    """Gate instance
+    This keeps track of all the gate methods (functions) and the related status/vaiables
+    """
     current_state = 'unknown'
-    
+
     def __init__(self):
         self.out_button = ''
         self.in_button = ''
@@ -15,15 +20,28 @@ class Gate():
         self.close()
 
     def close(self):
+        """Close the gate
+        """
         self.current_state = 'closing'
 
     def open(self):
+        """Open the gate
+        """
         self.current_state = 'opening'
-        
+
+    def stop(self):
+        """Stop the gate
+        """
+
+def setup():
+    """Setup to be run once at start of program
+    """
 
 def main_loop():
-
-    
+    """Main loop
+    Similair to the MainLoop() on an arduino, this will loop through indefinately,
+    calling all required inputs and outputs to make the gate function
+    """
 
 
 if __name__ == '__main__':
