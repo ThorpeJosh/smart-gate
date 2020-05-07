32b737bebb5941f726bca3f704c2e3df83d4c9d4
Josh Thorpe <josh@thorpe.engineering>
2020-05-08 01:05:19 +1000

    Added config.py configuration module

---
+++ src/config.py
@@ -0,0 +1,19 @@
+"""Module to store all configurable values and variables.
+"""
+
+# Board Pin numbers
+MOTORPIN0 = 16
+MOTORPIN1 = 18
+
+BUTTON_OUTSIDE_PIN = 8
+BUTTON_INSIDE_PIN = 10
+BUTTON_BOX_PIN = 12
+
+SHUNT_PIN = 0
+BATTERY_VOLTAGE_PIN = 3
+
+# Parameters (time in seconds unless stated otherwise)
+SHUNT_THRESHOLD = 500
+SHUNT_READ_DELAY = 0.8
+MAX_TIME_TO_OPEN_CLOSE = 15
+HOLD_OPEN_TIME = 10
