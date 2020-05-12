4cedf0fac6040b5c5435fc94736f7b20b4ad2ccf
Josh Thorpe <josh@thorpe.engineering>
2020-05-12 22:21:43 +1000

    Battery voltage correction factor

--- src/config.py
+++ src/config.py
@@ -18,5 +18,7 @@ SHUNT_READ_DELAY = 0.8
 MAX_TIME_TO_OPEN_CLOSE = 15
 HOLD_OPEN_TIME = 10
 
+BATTERY_VOLTAGE_CORRECTION = 10.75 # 10k 1k voltage divider
+
 # Commands that the gate needs to be able to handle on the job queue
 VALID_COMMANDS = ['open', 'close', 'openp', 'closep', 'resume']
