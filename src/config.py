e7446abd64f746631496ebc992895f84f50eeca7
Josh Thorpe <josh@thorpe.engineering>
2020-05-12 22:27:30 +1000

    Shunt Calibration

--- src/config.py
+++ src/config.py
@@ -13,12 +13,12 @@ SHUNT_PIN = 0
 BATTERY_VOLTAGE_PIN = 3
 
 # Parameters (time in seconds unless stated otherwise)
-SHUNT_THRESHOLD = 500
+SHUNT_THRESHOLD = 0.03 #Volts
 SHUNT_READ_DELAY = 0.8
 MAX_TIME_TO_OPEN_CLOSE = 15
 HOLD_OPEN_TIME = 10
 
-BATTERY_VOLTAGE_CORRECTION = 10.75 # 10k 1k voltage divider
+BATTERY_VOLTAGE_CORRECTION_FACTOR = 10.75 # 10k 1k voltage divider
 
 # Commands that the gate needs to be able to handle on the job queue
 VALID_COMMANDS = ['open', 'close', 'openp', 'closep', 'resume']
