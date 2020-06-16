727ced3be3f9b99fcee3f1de282c8b946afa3e56
Josh Thorpe <josh@thorpe.engineering>
2020-06-17 03:31:59 +1000

    Adjust voltage log voltages

--- src/battery_voltage_log.py
+++ src/battery_voltage_log.py
@@ -34,9 +34,9 @@ class BatteryVoltageLog():
         """scheduled job
         """
         bat_volt = round(self.battery_pin.voltage() * config.BATTERY_VOLTAGE_CORRECTION_FACTOR, 1)
-        if 25 <= bat_volt <= 29.6:
+        if 24.5 <= bat_volt <= 29.6:
             self.bat_logger.info("%sv", bat_volt)
-        elif 24.5 < bat_volt < 25:
+        elif 24 < bat_volt < 24.5:
             self.bat_logger.warning("%sv", bat_volt)
             root_logger.warning("Battery voltage: %sv", bat_volt)
         else:
