aed5b46a46e93c97062e08d2597d8b030fbfcc66
Josh Thorpe <josh@thorpe.engineering>
2020-07-20 02:41:12 +1000

    Add checksum to analog voltages

--- arduino_src/GateSketch/GateSketch.ino
+++ arduino_src/GateSketch/GateSketch.ino
@@ -9,21 +9,25 @@ Serial communication contract as follows:
 Handshake:
     Arduino sends 'A', RPi responds with 'A'
 Analog Voltages:
-    RPi sends 'V', Arduino responds with 'V' followed by voltages[]
+    RPi sends 'V', Arduino responds with 'V' followed by voltages[] and then a checksum
 Gate Trigger:
     Arduino sends 'O', followed trigger message, RPi doesn't respond.
 
 */
 
 const int noOfAnalogPins = 6;
+const int voltageDecimalPlaces = 4;
 float voltages[noOfAnalogPins];
+float checksum;
+byte incomingByte;
+
 
 void setup()
 {
     // Use external analog reference and put a jumper between pins 3.3V and AREF
     analogReference(EXTERNAL);
-    // start serial at 250000bps
-    Serial.begin(250000);
+    // start serial at 115200bps
+    Serial.begin(115200);
     serialHandshake();
 }
 
@@ -33,20 +37,36 @@ void loop()
     // Read analog pins and send data if requested over serial
     if (Serial.available() > 0)
     {
-        // Flush input buffer
-        Serial.read();
-        updateAnalogVoltages();
-        for(int i=0; i<noOfAnalogPins; i++)
+        incomingByte = Serial.read();
+        if(incomingByte == 'V')
         {
-            Serial.println(voltages[i], 4);
+            sendAnalogVoltages();
         }
     }
+}
+
+void flushSerialInputBuffer()
+{
+    while (Serial.available() > 0)
+    {
+        Serial.read();
+    }
+}
+
+void sendAnalogVoltages()
+{
+    updateAnalogVoltages();
+    Serial.println('V');
+    for(int i=0; i<noOfAnalogPins; i++)
+    {
+        Serial.println(voltages[i], voltageDecimalPlaces);
+    }
+    Serial.println(checksum, voltageDecimalPlaces);
 }
 
 void updateAnalogVoltages()
 {
     // Update the global voltages array
-
     int smoothSamples = 20;
 
     // Set all Analog values to zero
@@ -70,6 +90,14 @@ void updateAnalogVoltages()
     {
         voltages[i] = (voltages[i] / smoothSamples) * (3.3 / 1023.0);
     }
+
+    // Calculate the checksum
+    checksum = 0.0;
+    for(int i=0; i<noOfAnalogPins; i++)
+    {
+        // Round the voltages to the correct decimal places
+        checksum += round(voltages[i]*float(pow(10, voltageDecimalPlaces)))/float(pow(10, voltageDecimalPlaces));
+    }
 }
 
 
@@ -81,9 +109,5 @@ void serialHandshake()
         Serial.println('A');
         delay(200);
     }
-    // Clear buffer
-    while (Serial.available() > 0)
-    {
-        Serial.read();
-    }
+    flushSerialInputBuffer();
 }
