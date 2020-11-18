e79f56f1c72dacf3ef2ccede70c3f2c73c9e0c02
Josh Thorpe <josh@thorpe.engineering>
2020-11-19 00:07:18 +1100

    Add camera queue and servo control to camera class

--- rpi_src/camera.py
+++ rpi_src/camera.py
@@ -6,18 +6,32 @@ import datetime
 import time
 import logging
 import queue
+import threading
 from picamera import PiCamera
 
 from config import Config as config
+from serial_analog import ArduinoInterface
 
 logger = logging.getLogger('root')
 
 class Camera():
+    """ Class to handle operations of the camera """
+    def __init__(self):
+        # setup camera queue and start a thread to read it and handle the camera
+        self.camera_q = queue.Queue()
+        threading.Thread(target=self._read_queue, daemon=True).start()
 
-    def __init__():
-        pass
+    @staticmethod
+    def move_servo(position):
+        """ Request arduino to move servo to position
+        position: a value between 0 and 180 """
+        assert 0 <= position <= 180
+        ArduinoInterface.ser.write("S".encode())
+        ArduinoInterface.ser.write(position.to_bytes(1, byteorder='little'))
 
-    def take_picture(self):
+
+    @staticmethod
+    def take_picture():
         """ Method to take a picture using the rpi camera
         """
         # Record datetime for filename
@@ -29,6 +43,8 @@ class Camera():
             str(now.hour).zfill(2),
             str(now.minute).zfill(2),
             str(now.second).zfill(2))
+        filename = os.path.join(config.CAMERA_SAVE_PATH, '{}.jpg'.format(datetime_string))
+        logger.debug("Taking a picture: %s", filename)
 
         # Create camera objects
         camera = PiCamera()
@@ -36,6 +52,23 @@ class Camera():
 
         camera.start_preview()
         time.sleep(5)
-        camera.capture(os.path.join(config.RECORDING_PATH, '{}.jpg'.format(self.datetime_string)))
+        camera.capture(filename)
         camera.close()
 
+    def _read_queue(self):
+        """ Method that monitors camera queue and takes pictures when requested
+        """
+        while True:
+            job = self.camera_q.get()
+            # Exit thread gracefully with a 'kill' command
+            if job == 'kill':
+                logger.warning('received kill command on camera queue')
+                return
+            if job == 'inside':
+                self.move_servo(config.CAMERA_INSIDE_ANGLE)
+                self.take_picture()
+            elif job == 'outside':
+                self.move_servo(config.CAMERA_OUTSIDE_ANGLE)
+                self.take_picture()
+            else:
+                logger.warning("Received invalid command on camera queue")
