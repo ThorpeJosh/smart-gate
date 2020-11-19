2e06c52f1b7c1227a8fce0f05c5cdd92df0a3bc5
Josh Thorpe <josh@thorpe.engineering>
2020-11-19 18:39:45 +1100

    Upgrade to latest pylint linter

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     include_package_data=True,
     install_requires=['pyserial>=3.4', 'pathlib==1.0.1', 'schedule>=0.6.0', 'gpiozero>=1.5.0',
                       'RPI.GPIO>=0.7.0', 'picamera>=1.13', 'jsonschema>=3.0.0'],
-    extras_require={"dev": ["pytest>=6.0.0", "pylint<=2.5.3", "tox>= 3.0.0"]},
+    extras_require={"dev": ["pytest>=6.0.0", "pylint>=2.6.0", "tox>= 3.0.0"]},
     python_requires='>=3.5',
     classifiers=[
         'License :: OSI Approved :: MIT License',
