0e8ad8984af32cb59370523d74e2306c6cdf7446
Josh Thorpe <josh@thorpe.engineering>
2021-03-04 20:32:58 +1100

    arduino-cli changed default compile behaviour to include --dry-run

--- arduino_src/verify.sh
+++ arduino_src/verify.sh
@@ -3,4 +3,4 @@
 relative_path_to_this_script=`dirname $0`
 
 # Compile without producing build files
-arduino-cli compile -v --dry-run -b arduino:avr:uno $relative_path_to_this_script/GateSketch
+arduino-cli compile -v -b arduino:avr:uno $relative_path_to_this_script/GateSketch
