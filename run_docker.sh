7be30578a4bfaeee006943d449d021a16fc79050
Josh Thorpe <josh@thorpe.engineering>
2021-03-11 00:54:43 +1100

    Give container access to system devices

--- run_docker.sh
+++ run_docker.sh
@@ -4,4 +4,5 @@ docker run -d \
     -v ~/.config/smart-gate:/root/.config/smart-gate \
     -v ~/gate-media:/root/gate-media \
     -v ~/pipe:/root/pipe \
+    --priveliged -v /dev:/dev \
     smart-gate:latest
