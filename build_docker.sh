6d51aaa1916f39ac5b486845671303b2efb1e6d8
thorpejosh <josh@thorpe.engineering>
2022-10-21 17:14:26 +0800

    cicd: multistage docker builds for prod and dev

--- build_docker.sh
+++ build_docker.sh
@@ -4,7 +4,21 @@ VERSION="1.0.0"
 echo "Building smart-gate docker version:$VERSION"
 docker build \
     --build-arg VERSION=$VERSION \
+    --build-arg BUILDKIT_INLINE_CACHE=1 \
     -t thorpejosh/smart-gate:$VERSION \
     -t thorpejosh/smart-gate:latest \
-    --no-cache --rm \
+    --platform linux/arm/v7 \
+    --cache-from thorpejosh/smart-gate:latest \
+    --target prod \
+    - < Dockerfile
+
+VERSION="$VERSION-dev"
+docker build \
+    --build-arg VERSION=$VERSION \
+    --build-arg BUILDKIT_INLINE_CACHE=1 \
+    -t thorpejosh/smart-gate:$VERSION \
+    -t thorpejosh/smart-gate:latest-dev \
+    --platform linux/arm/v7 \
+    --cache-from thorpejosh/smart-gate:latest \
+    --target dev \
     - < Dockerfile
