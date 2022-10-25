2332d25e300e973f387bfae768d60e72af248a3c
thorpejosh <thorpe.josh@outlook.com>
2022-10-26 00:56:30 +0800

    cicd: update docker build script

--- build_docker.sh
+++ build_docker.sh
@@ -4,21 +4,8 @@ VERSION="1.0.0"
 echo "Building smart-gate docker version:$VERSION"
 docker build \
     --build-arg VERSION=$VERSION \
-    --build-arg BUILDKIT_INLINE_CACHE=1 \
     -t thorpejosh/smart-gate:$VERSION \
     -t thorpejosh/smart-gate:latest \
-    --platform linux/arm/v7 \
-    --cache-from thorpejosh/smart-gate:latest \
-    --target prod \
-    - < Dockerfile
-
-VERSION="$VERSION-dev"
-docker build \
-    --build-arg VERSION=$VERSION \
-    --build-arg BUILDKIT_INLINE_CACHE=1 \
-    -t thorpejosh/smart-gate:$VERSION \
-    -t thorpejosh/smart-gate:latest-dev \
-    --platform linux/arm/v7 \
-    --cache-from thorpejosh/smart-gate:latest \
-    --target dev \
+    --pull \
+    #--platform linux/arm/v7 \
     - < Dockerfile
